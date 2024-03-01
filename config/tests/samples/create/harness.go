9de964e5c0bf4936a355d68a99f7570b08054d6b
justinsb <justinsb@google.com>
2024-03-01 11:43:31 -0500

    tests: always normalize golden output
    Otherwise we were not normalizing when first creating the golden
    output.

--- config/tests/samples/create/harness.go
+++ config/tests/samples/create/harness.go
@@ -505,13 +505,16 @@ func (h *Harness) waitForCRDReady(obj client.Object) {
 }
 
 func (h *Harness) CompareGoldenFile(p string, got string, normalizers ...func(s string) string) {
+	for _, normalizer := range normalizers {
+		got = normalizer(got)
+	}
+
 	if os.Getenv("WRITE_GOLDEN_OUTPUT") != "" {
 		// Short-circuit when the output is correct
 		b, err := os.ReadFile(p)
 		if err == nil {
 			want := string(b)
 			for _, normalizer := range normalizers {
-				got = normalizer(got)
 				want = normalizer(want)
 			}
 			if want == got {
@@ -527,7 +530,6 @@ func (h *Harness) CompareGoldenFile(p string, got string, normalizers ...func(s 
 		want := string(h.MustReadFile(p))
 
 		for _, normalizer := range normalizers {
-			got = normalizer(got)
 			want = normalizer(want)
 		}
 
