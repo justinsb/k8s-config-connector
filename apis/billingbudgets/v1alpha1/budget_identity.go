a418bb75d9cebbba4189128ae6b7c6c34396dff9
justinsb <justinsb@google.com>
2025-02-25 12:14:24 -0500

    BillingBudget: Identity & Reference not yet needed

--- apis/billingbudgets/v1alpha1/budget_identity.go
+++ apis/billingbudgets/v1alpha1/budget_identity.go
@@ -14,104 +14,104 @@
 
 package v1alpha1
 
-import (
-	"context"
-	"fmt"
-	"strings"
+// import (
+// 	"context"
+// 	"fmt"
+// 	"strings"
 
-	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/common"
-	refsv1beta1 "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-)
+// 	"github.com/GoogleCloudPlatform/k8s-config-connector/apis/common"
+// 	refsv1beta1 "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+// 	"sigs.k8s.io/controller-runtime/pkg/client"
+// )
 
-// BudgetIdentity defines the resource reference to BillingBudget, which "External" field
-// holds the GCP identifier for the KRM object.
-type BudgetIdentity struct {
-	parent *BudgetParent
-	id     string
-}
+// // BudgetIdentity defines the resource reference to BillingBudget, which "External" field
+// // holds the GCP identifier for the KRM object.
+// type BudgetIdentity struct {
+// 	parent *BudgetParent
+// 	id     string
+// }
 
-func (i *BudgetIdentity) String() string {
-	return i.parent.String() + "/budgets/" + i.id
-}
+// func (i *BudgetIdentity) String() string {
+// 	return i.parent.String() + "/budgets/" + i.id
+// }
 
-func (i *BudgetIdentity) ID() string {
-	return i.id
-}
+// func (i *BudgetIdentity) ID() string {
+// 	return i.id
+// }
 
-func (i *BudgetIdentity) Parent() *BudgetParent {
-	return i.parent
-}
+// func (i *BudgetIdentity) Parent() *BudgetParent {
+// 	return i.parent
+// }
 
-type BudgetParent struct {
-	ProjectID string
-	Location  string
-}
+// type BudgetParent struct {
+// 	ProjectID string
+// 	Location  string
+// }
 
-func (p *BudgetParent) String() string {
-	return "projects/" + p.ProjectID + "/locations/" + p.Location
-}
+// func (p *BudgetParent) String() string {
+// 	return "projects/" + p.ProjectID + "/locations/" + p.Location
+// }
 
-// New builds a BudgetIdentity from the Config Connector Budget object.
-func NewBudgetIdentity(ctx context.Context, reader client.Reader, obj *BillingBudget) (*BudgetIdentity, error) {
+// // New builds a BudgetIdentity from the Config Connector Budget object.
+// func NewBudgetIdentity(ctx context.Context, reader client.Reader, obj *BillingBudget) (*BudgetIdentity, error) {
 
-	// Get Parent
-	projectRef, err := refsv1beta1.ResolveProject(ctx, reader, obj.GetNamespace(), obj.Spec.ProjectRef)
-	if err != nil {
-		return nil, err
-	}
-	projectID := projectRef.ProjectID
-	if projectID == "" {
-		return nil, fmt.Errorf("cannot resolve project")
-	}
-	location := obj.Spec.Location
+// 	// Get Parent
+// 	projectRef, err := refsv1beta1.ResolveProject(ctx, reader, obj.GetNamespace(), obj.Spec.ProjectRef)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	projectID := projectRef.ProjectID
+// 	if projectID == "" {
+// 		return nil, fmt.Errorf("cannot resolve project")
+// 	}
+// 	location := obj.Spec.Location
 
-	// Get desired ID
-	resourceID := common.ValueOf(obj.Spec.ResourceID)
-	if resourceID == "" {
-		resourceID = obj.GetName()
-	}
-	if resourceID == "" {
-		return nil, fmt.Errorf("cannot resolve resource ID")
-	}
+// 	// Get desired ID
+// 	resourceID := common.ValueOf(obj.Spec.ResourceID)
+// 	if resourceID == "" {
+// 		resourceID = obj.GetName()
+// 	}
+// 	if resourceID == "" {
+// 		return nil, fmt.Errorf("cannot resolve resource ID")
+// 	}
 
-	// Use approved External
-	externalRef := common.ValueOf(obj.Status.ExternalRef)
-	if externalRef != "" {
-		// Validate desired with actual
-		actualParent, actualResourceID, err := ParseBudgetExternal(externalRef)
-		if err != nil {
-			return nil, err
-		}
-		if actualParent.ProjectID != projectID {
-			return nil, fmt.Errorf("spec.projectRef changed, expect %s, got %s", actualParent.ProjectID, projectID)
-		}
-		if actualParent.Location != location {
-			return nil, fmt.Errorf("spec.location changed, expect %s, got %s", actualParent.Location, location)
-		}
-		if actualResourceID != resourceID {
-			return nil, fmt.Errorf("cannot reset `metadata.name` or `spec.resourceID` to %s, since it has already assigned to %s",
-				resourceID, actualResourceID)
-		}
-	}
-	return &BudgetIdentity{
-		parent: &BudgetParent{
-			ProjectID: projectID,
-			Location:  location,
-		},
-		id: resourceID,
-	}, nil
-}
+// 	// Use approved External
+// 	externalRef := common.ValueOf(obj.Status.ExternalRef)
+// 	if externalRef != "" {
+// 		// Validate desired with actual
+// 		actualParent, actualResourceID, err := ParseBudgetExternal(externalRef)
+// 		if err != nil {
+// 			return nil, err
+// 		}
+// 		if actualParent.ProjectID != projectID {
+// 			return nil, fmt.Errorf("spec.projectRef changed, expect %s, got %s", actualParent.ProjectID, projectID)
+// 		}
+// 		if actualParent.Location != location {
+// 			return nil, fmt.Errorf("spec.location changed, expect %s, got %s", actualParent.Location, location)
+// 		}
+// 		if actualResourceID != resourceID {
+// 			return nil, fmt.Errorf("cannot reset `metadata.name` or `spec.resourceID` to %s, since it has already assigned to %s",
+// 				resourceID, actualResourceID)
+// 		}
+// 	}
+// 	return &BudgetIdentity{
+// 		parent: &BudgetParent{
+// 			ProjectID: projectID,
+// 			Location:  location,
+// 		},
+// 		id: resourceID,
+// 	}, nil
+// }
 
-func ParseBudgetExternal(external string) (parent *BudgetParent, resourceID string, err error) {
-	tokens := strings.Split(external, "/")
-	if len(tokens) != 6 || tokens[0] != "projects" || tokens[2] != "locations" || tokens[4] != "budgets" {
-		return nil, "", fmt.Errorf("format of BillingBudget external=%q was not known (use projects/{{projectID}}/locations/{{location}}/budgets/{{budgetID}})", external)
-	}
-	parent = &BudgetParent{
-		ProjectID: tokens[1],
-		Location:  tokens[3],
-	}
-	resourceID = tokens[5]
-	return parent, resourceID, nil
-}
+// func ParseBudgetExternal(external string) (parent *BudgetParent, resourceID string, err error) {
+// 	tokens := strings.Split(external, "/")
+// 	if len(tokens) != 6 || tokens[0] != "projects" || tokens[2] != "locations" || tokens[4] != "budgets" {
+// 		return nil, "", fmt.Errorf("format of BillingBudget external=%q was not known (use projects/{{projectID}}/locations/{{location}}/budgets/{{budgetID}})", external)
+// 	}
+// 	parent = &BudgetParent{
+// 		ProjectID: tokens[1],
+// 		Location:  tokens[3],
+// 	}
+// 	resourceID = tokens[5]
+// 	return parent, resourceID, nil
+// }
--- apis/billingbudgets/v1alpha1/budget_reference.go
+++ apis/billingbudgets/v1alpha1/budget_reference.go
@@ -14,70 +14,70 @@
 
 package v1alpha1
 
-import (
-	"context"
-	"fmt"
+// import (
+// 	"context"
+// 	"fmt"
 
-	refsv1beta1 "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
-	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/k8s"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-)
+// 	refsv1beta1 "github.com/GoogleCloudPlatform/k8s-config-connector/apis/refs/v1beta1"
+// 	"github.com/GoogleCloudPlatform/k8s-config-connector/pkg/k8s"
+// 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+// 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+// 	"k8s.io/apimachinery/pkg/types"
+// 	"sigs.k8s.io/controller-runtime/pkg/client"
+// )
 
-var _ refsv1beta1.ExternalNormalizer = &BudgetRef{}
+// var _ refsv1beta1.ExternalNormalizer = &BudgetRef{}
 
-// BudgetRef defines the resource reference to BillingBudget, which "External" field
-// holds the GCP identifier for the KRM object.
-type BudgetRef struct {
-	// A reference to an externally managed BillingBudget resource.
-	// Should be in the format "projects/{{projectID}}/locations/{{location}}/budgets/{{budgetID}}".
-	External string `json:"external,omitempty"`
+// // BudgetRef defines the resource reference to BillingBudget, which "External" field
+// // holds the GCP identifier for the KRM object.
+// type BudgetRef struct {
+// 	// A reference to an externally managed BillingBudget resource.
+// 	// Should be in the format "projects/{{projectID}}/locations/{{location}}/budgets/{{budgetID}}".
+// 	External string `json:"external,omitempty"`
 
-	// The name of a BillingBudget resource.
-	Name string `json:"name,omitempty"`
+// 	// The name of a BillingBudget resource.
+// 	Name string `json:"name,omitempty"`
 
-	// The namespace of a BillingBudget resource.
-	Namespace string `json:"namespace,omitempty"`
-}
+// 	// The namespace of a BillingBudget resource.
+// 	Namespace string `json:"namespace,omitempty"`
+// }
 
-// NormalizedExternal provision the "External" value for other resource that depends on BillingBudget.
-// If the "External" is given in the other resource's spec.BillingBudgetRef, the given value will be used.
-// Otherwise, the "Name" and "Namespace" will be used to query the actual BillingBudget object from the cluster.
-func (r *BudgetRef) NormalizedExternal(ctx context.Context, reader client.Reader, otherNamespace string) (string, error) {
-	if r.External != "" && r.Name != "" {
-		return "", fmt.Errorf("cannot specify both name and external on %s reference", BillingBudgetGVK.Kind)
-	}
-	// From given External
-	if r.External != "" {
-		if _, _, err := ParseBudgetExternal(r.External); err != nil {
-			return "", err
-		}
-		return r.External, nil
-	}
+// // NormalizedExternal provision the "External" value for other resource that depends on BillingBudget.
+// // If the "External" is given in the other resource's spec.BillingBudgetRef, the given value will be used.
+// // Otherwise, the "Name" and "Namespace" will be used to query the actual BillingBudget object from the cluster.
+// func (r *BudgetRef) NormalizedExternal(ctx context.Context, reader client.Reader, otherNamespace string) (string, error) {
+// 	if r.External != "" && r.Name != "" {
+// 		return "", fmt.Errorf("cannot specify both name and external on %s reference", BillingBudgetGVK.Kind)
+// 	}
+// 	// From given External
+// 	if r.External != "" {
+// 		if _, _, err := ParseBudgetExternal(r.External); err != nil {
+// 			return "", err
+// 		}
+// 		return r.External, nil
+// 	}
 
-	// From the Config Connector object
-	if r.Namespace == "" {
-		r.Namespace = otherNamespace
-	}
-	key := types.NamespacedName{Name: r.Name, Namespace: r.Namespace}
-	u := &unstructured.Unstructured{}
-	u.SetGroupVersionKind(BillingBudgetGVK)
-	if err := reader.Get(ctx, key, u); err != nil {
-		if apierrors.IsNotFound(err) {
-			return "", k8s.NewReferenceNotFoundError(u.GroupVersionKind(), key)
-		}
-		return "", fmt.Errorf("reading referenced %s %s: %w", BillingBudgetGVK, key, err)
-	}
-	// Get external from status.externalRef. This is the most trustworthy place.
-	actualExternalRef, _, err := unstructured.NestedString(u.Object, "status", "externalRef")
-	if err != nil {
-		return "", fmt.Errorf("reading status.externalRef: %w", err)
-	}
-	if actualExternalRef == "" {
-		return "", k8s.NewReferenceNotReadyError(u.GroupVersionKind(), key)
-	}
-	r.External = actualExternalRef
-	return r.External, nil
-}
+// 	// From the Config Connector object
+// 	if r.Namespace == "" {
+// 		r.Namespace = otherNamespace
+// 	}
+// 	key := types.NamespacedName{Name: r.Name, Namespace: r.Namespace}
+// 	u := &unstructured.Unstructured{}
+// 	u.SetGroupVersionKind(BillingBudgetGVK)
+// 	if err := reader.Get(ctx, key, u); err != nil {
+// 		if apierrors.IsNotFound(err) {
+// 			return "", k8s.NewReferenceNotFoundError(u.GroupVersionKind(), key)
+// 		}
+// 		return "", fmt.Errorf("reading referenced %s %s: %w", BillingBudgetGVK, key, err)
+// 	}
+// 	// Get external from status.externalRef. This is the most trustworthy place.
+// 	actualExternalRef, _, err := unstructured.NestedString(u.Object, "status", "externalRef")
+// 	if err != nil {
+// 		return "", fmt.Errorf("reading status.externalRef: %w", err)
+// 	}
+// 	if actualExternalRef == "" {
+// 		return "", k8s.NewReferenceNotReadyError(u.GroupVersionKind(), key)
+// 	}
+// 	r.External = actualExternalRef
+// 	return r.External, nil
+// }
