2e08901629926d007b32d7bb5b171a5281d5cc70
justinsb <justinsb@google.com>
2025-03-12 17:34:18 -0400

    DiscoveryEngineDataStoreTargetSite: Support for testing with mockgcp

--- config/tests/samples/create/harness.go
+++ config/tests/samples/create/harness.go
@@ -838,6 +838,8 @@ func MaybeSkip(t *testing.T, name string, resources []*unstructured.Unstructured
 			case schema.GroupKind{Group: "edgenetwork.cnrm.cloud.google.com", Kind: "EdgeNetworkNetwork"}:
 			case schema.GroupKind{Group: "edgenetwork.cnrm.cloud.google.com", Kind: "EdgeNetworkSubnet"}:
 
+			case schema.GroupKind{Group: "discoveryengine.cnrm.cloud.google.com", Kind: "DiscoveryEngineDataStoreTargetSite"}:
+
 			case schema.GroupKind{Group: "firestore.cnrm.cloud.google.com", Kind: "FirestoreDatabase"}:
 
 			case schema.GroupKind{Group: "kms.cnrm.cloud.google.com", Kind: "KMSKeyRing"}:
